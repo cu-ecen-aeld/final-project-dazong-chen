bc4b56a7fc807e3a7e1dde31298febce4cf0af79
dazong chen <sunnysimon1995@gmail.com>
2021-11-07 11:47:03 -0700

    A4

---
+++ final_buildroot/assignment-autotest/test/assignment2/assignment-test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+pushd $(dirname $0)
+source script-helpers
+
+SCRIPTS_DIR=$(pwd)
+SOURCE_DIR=$(realpath ${SCRIPTS_DIR}/../../../)
+
+pushd ${SOURCE_DIR}/finder-app
+
+make clean
+make
+
+./writer
+rc=$?
+if [ $rc -ne 1 ]; then
+	add_validate_error "writer should have exited with return value 1 if no parameters were specified"
+fi
+
+./writer "$filedir"
+rc=$?
+if [ $rc -ne 1 ]; then
+	add_validate_error "writer should have exited with return value 1 if write string is not specified"
+fi
+
+./finder-test.sh
+rc=$?
+if [ $rc -ne 0 ]; then
+	add_validate_error "finder-test.sh execution failed with return code $rc"
+fi
