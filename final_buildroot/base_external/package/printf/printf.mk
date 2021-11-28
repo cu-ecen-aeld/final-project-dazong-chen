9d93d7df3a51715458c24ac248b17cedaea8f4fa
dazong chen <sunnysimon1995@gmail.com>
2021-11-28 13:09:08 -0700

    temperature sensor

--- final_buildroot/base_external/package/printf/printf.mk
+++ final_buildroot/base_external/package/printf/printf.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your printf git contents
-PRINTF_VERSION = 2f90b9905a57dd7198c6c04629b7f802399539b3
+PRINTF_VERSION = 4f71fa283e3de21a1271b2351574b2504199b0da
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
