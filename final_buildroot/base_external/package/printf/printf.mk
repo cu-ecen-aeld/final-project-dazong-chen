f631917963fc153e0904f79a8e67f3d9a3cf8b24
dazong chen <sunnysimon1995@gmail.com>
2021-11-30 14:35:58 -0700

    BB network setting

--- final_buildroot/base_external/package/printf/printf.mk
+++ final_buildroot/base_external/package/printf/printf.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your printf git contents
-PRINTF_VERSION = 4f71fa283e3de21a1271b2351574b2504199b0da
+PRINTF_VERSION = 4cbe9e5e1d17dad9324287d38f576b3b136d4dc1
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,10 +15,13 @@ PRINTF_GIT_SUBMODULES = YES
 
 define PRINTF_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/final_application/i2c all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/final_application/client_server all
 endef
 
 define PRINTF_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/final_application/i2c/main $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/final_application/client_server/client $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/final_application/client_server/server $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
