67bfb2ed9cec25955e00234a7d3e1f3a86bc3d2e
dazong chen <sunnysimon1995@gmail.com>
2021-12-04 20:20:54 -0700

    mk file

---
+++ final_buildroot/base_external/package/final_project/final_project.mk
@@ -0,0 +1,26 @@
+##############################################################
+#
+# AESD-ASSIGNMENTS
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your printf git contents
+FINAL_PROJECT_VERSION = 6901bc5d4c7e99af32aae329ab2837bf5f6a4e9a
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+FINAL_PROJECT_SITE = git@github.com:cu-ecen-aeld/final-project-dazong-chen.git
+FINAL_PROJECT_SITE_METHOD = git
+FINAL_PROJECT_GIT_SUBMODULES = YES
+
+define FINAL_PROJECT_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/final_application/htu21d_sensor all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/final_application/client_server all
+endef
+
+define FINAL_PROJECT_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/final_application/client $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/final_application/server $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(generic-package))
