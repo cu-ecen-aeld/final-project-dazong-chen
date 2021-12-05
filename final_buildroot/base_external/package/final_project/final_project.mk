9976e3d1d9fb9fae81a17450fe53ae922e060922
dazong chen <sunnysimon1995@gmail.com>
2021-12-05 10:44:57 -0700

    remove .o

--- final_buildroot/base_external/package/final_project/final_project.mk
+++ final_buildroot/base_external/package/final_project/final_project.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your printf git contents
-FINAL_PROJECT_VERSION = 2655a2be3b16b4efc9aafc5954bc07d2f5388ce4
+FINAL_PROJECT_VERSION = f1a26e14c664ae6871b90ec921d52bbda55a594e
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -14,12 +14,7 @@ FINAL_PROJECT_SITE_METHOD = git
 FINAL_PROJECT_GIT_SUBMODULES = YES
 
 define FINAL_PROJECT_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/final_application/client all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/final_application/client/obj all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/final_application/server all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/final_application/server/obj all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/final_application/htu21d_sensor all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/final_application/htu21d_sensor/obj all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/final_application all
 endef
 
 define FINAL_PROJECT_INSTALL_TARGET_CMDS
