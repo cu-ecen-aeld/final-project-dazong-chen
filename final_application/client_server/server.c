2e19cbe872ccc33e5852891e740dddf7cf38a118
dazong chen <sunnysimon1995@gmail.com>
2021-12-05 07:24:46 -0700

    makefile

--- final_application/client_server/server.c
+++ final_application/client_server/server.c
@@ -16,12 +16,23 @@
 #include <arpa/inet.h>
 #include <sys/wait.h>
 #include <signal.h>
-#include "../htu21d_sensor/htu21d_sensor.h"
+#include <sys/stat.h>
+#include <linux/i2c-dev.h>
+#include <syslog.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <fcntl.h>
 
 #define		PORT			"3490"  	// the port users will be connecting to
 #define 	BACKLOG 		10	 	// how many pending connections queue will hold
 #define		MAX_DATA_BYTES		100
 //#define		MAX_TEMP_DATA_BYTES	10
+
+#define 	TEMP_SENSOR_ADDR			0x40
+#define		I2C_DEVICE_FILE          		"/dev/i2c-2"
+#define		TEMPERATURE_MEASUREMENT_NO_HOLD		0xF3
+#define		STATUS_BITS				0x03     // The two status bits, must be set to 0 before calculating physical values
+#define		TEMP_DATA_BYTES				2
 
 void sigchld_handler(int s)
 {
@@ -45,6 +56,8 @@ void *get_in_addr(struct sockaddr *sa)
 
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
+
+float read_temperature();
 
 int main(void)
 {
@@ -176,5 +189,52 @@ int main(void)
 	}
 
 	return 0;
+}
+
+float read_temperature()
+{
+	int file = open(I2C_DEVICE_FILE, O_RDWR);
+	int i2c_addr = 0;
+	
+	if(file < 0)
+	{
+		syslog(LOG_ERR, "failed to open i2c-2\n");
+		printf("failed to open i2c-2\n");
+	}
+	
+	i2c_addr = TEMP_SENSOR_ADDR;
+	
+	// failed to specify with device address
+	if(ioctl(file, I2C_SLAVE, i2c_addr) < 0)
+	{
+		syslog(LOG_ERR, "failed to specify with device address");
+		printf("failed to specify with device address\n");
+	}
+
+	char buf[10] = {0};
+	
+	buf[0] = TEMPERATURE_MEASUREMENT_NO_HOLD;
+	
+	write(file, buf, 1);
+	
+	usleep(60000);	// measurement time
+	
+	char data_buf[TEMP_DATA_BYTES] = {0};
+	int rc = read(file, data_buf, TEMP_DATA_BYTES);
+	
+	if( rc != 2)
+	{
+		syslog(LOG_ERR, "i2c read transaction failed %d\n",rc);
+		printf("i2c read transaction failed %d\n",rc);
+	}
+	
+	// calculate temperature value
+	uint16_t	val = 0;
+	float		temp = 0;
+	
+	val = ( (data_buf[0] << 8 | data_buf[1]) &~ STATUS_BITS);
+	temp = -46.85 + (175.72*val/65536);
+	
+	return 	temp;
 }
 
--- final_buildroot/base_external/package/final_project/final_project.mk
+++ final_buildroot/base_external/package/final_project/final_project.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your printf git contents
-FINAL_PROJECT_VERSION = 6901bc5d4c7e99af32aae329ab2837bf5f6a4e9a
+FINAL_PROJECT_VERSION = 0f5eda968af7fa71e29921fd03eba3669f662d94
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -14,8 +14,6 @@ FINAL_PROJECT_SITE_METHOD = git
 FINAL_PROJECT_GIT_SUBMODULES = YES
 
 define FINAL_PROJECT_BUILD_CMDS
-	#$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/final_application/htu21d_sensor all
-	#$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/final_application/client_server all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/final_application all
 endef
 
