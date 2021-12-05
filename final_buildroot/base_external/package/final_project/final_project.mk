365d87996f85557d418e722066e7bc7bab109bf8
dazong chen <sunnysimon1995@gmail.com>
2021-12-05 07:40:55 -0700

    unterminated quoted string

--- final_buildroot/base_external/package/final_project/final_project.mk
+++ final_buildroot/base_external/package/final_project/final_project.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your printf git contents
-FINAL_PROJECT_VERSION = 0f5eda968af7fa71e29921fd03eba3669f662d94
+FINAL_PROJECT_VERSION = 2e19cbe872ccc33e5852891e740dddf7cf38a118
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -14,12 +14,12 @@ FINAL_PROJECT_SITE_METHOD = git
 FINAL_PROJECT_GIT_SUBMODULES = YES
 
 define FINAL_PROJECT_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/final_application all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/final_application/client_server all
 endef
 
 define FINAL_PROJECT_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/final_application/client $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/final_application/server $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/final_application/client_server/client $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/final_application/client_server/server $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
